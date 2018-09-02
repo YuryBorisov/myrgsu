e08149d33b4c40b0c967d91893a922e08285b5f5
root <root@183125.simplecloud.ru>
2018-09-02 16:35:53 +0300

    fix message for send users

--- app/Console/Commands/SendMessage.php
+++ app/Console/Commands/SendMessage.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use App\Models\UserVK;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class SendMessage extends Command
 {
@@ -34,42 +36,32 @@ class SendMessage extends Command
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        foreach (User::all() as $user)
-        {
-            $arr = [
-                'schedule' => $user->call,
-                'broadcasting' => 0,
-                'news_project' => $user->distribution
-            ];
-            DB::table('users')->where('id', $user->id)->update(['notifications' => json_encode($arr)]);
-        }
-        /*
         $i = 0;
-        foreach (User::whereDistribution(0)->get() as $user)
+        //User::all()
+        $sleep = 4;
+        foreach (User::where('user_id', '=', 333114129)->get() as $user)
         {
-
-            if($i == 3)
+            if($i == 4)
             {
-                sleep(5);
+                $output->writeln([
+                   '<info>Sleep to ' . $sleep . ' seconds</info>'
+                ]);
+                sleep($sleep);
                 $i = 0;
             }
             Commands::sendMessage([
-                'message' => "Доброе утро {$user->first_name} \xE2\x9C\x8C\nБыл добавлен личный кабинет, теперь там '\xF0\x9F\x93\x85 Расписание' и ещё кое - что \xF0\x9F\x98\x89\nКоманды '\xF0\x9F\x9A\xAA Аудитории', '\xF0\x9F\x91\x94 Преподаватели', '\xF0\x9F\x93\xB0 Новости РГСУ' теперь работают.\n\xF0\x9F\x8E\xBA Вещание - новая команда в личном кабинете. Отправляй сообщения всем кто учиться на твоем факультете и не только.\nХорошего дня =]\n",
+                'message' => "Привет {$user->first_name} \xE2\x9C\x8C\nС началом учебного года \xF0\x9F\x8E\x93 \xF0\x9F\x8E\x89\nДумаю, что ты меня ещё не забыл \xF0\x9F\x92\xAC\nВыбирай свою группу и получай расписание в считанные секунды \xF0\x9F\x9A\x80\nРассказывай обо мне своим одногруппникам и всем друзьям из университету \xF0\x9F\x93\xA2\nУдачи в этом учебном году \xF0\x9F\x98\x89 \xF0\x9F\x98\x89",
                 'user_id' => $user['user_id'],
                 'access_token' => env('VK_BOT_KEY'),
-                'v' => '5.0'
+                'v' => '5.84'
             ]);
-            echo $user->user_id."\n";
+            $output->writeln([
+                '<info>Send to: ' . $user->first_name . ' ' . $user->last_name . ' [' . $user->user_id . ']'
+            ]);
             $i++;
         }
-        */
     }
 
 }
